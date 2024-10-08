61aed977a422c6cf2634cf14cee45e972e29dfaf
ruokun-niu <ruokunniu@microsoft.com>
2024-10-08 11:35:49 -0700

    cargo fmt

--- core/src/path_solver/mod.rs
+++ core/src/path_solver/mod.rs
@@ -12,7 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #![allow(dead_code)]
 pub mod match_path;
 pub mod solution;
--- query-perf/src/models/domain_model_graph.rs
+++ query-perf/src/models/domain_model_graph.rs
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 extern crate generational_arena;
 use drasi_core::models::ElementPropertyMap;
 use generational_arena::{Arena, Index};
