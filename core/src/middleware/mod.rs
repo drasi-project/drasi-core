b391fce26f66e166fecb482751038053f3830a4d
Daniel Gerlag <danielgerlag@microsoft.com>
2025-01-07 09:53:44 -0800

    Unwind middleware (#39)
    * unwind middleware
    
    * wip
    
    * cargo fmt
    
    * clippy

--- core/src/middleware/mod.rs
+++ core/src/middleware/mod.rs
@@ -15,7 +15,10 @@
 use std::{collections::HashMap, sync::Arc};
 
 use crate::{
-    interface::{MiddlewareError, MiddlewareSetupError, SourceMiddleware, SourceMiddlewareFactory},
+    interface::{
+        ElementIndex, MiddlewareError, MiddlewareSetupError, SourceMiddleware,
+        SourceMiddlewareFactory,
+    },
     models::{SourceChange, SourceMiddlewareConfig},
 };
 
@@ -102,13 +105,18 @@ impl SourceMiddlewarePipeline {
     pub async fn process(
         &self,
         source_change: SourceChange,
+        element_index: Arc<dyn ElementIndex>,
     ) -> Result<Vec<SourceChange>, MiddlewareError> {
         let mut source_changes = vec![source_change];
 
         for middleware in &self.pipeline {
             let mut new_source_changes = Vec::new();
             for source_change in source_changes {
-                new_source_changes.append(&mut middleware.process(source_change).await?);
+                new_source_changes.append(
+                    &mut middleware
+                        .process(source_change, element_index.as_ref())
+                        .await?,
+                );
             }
 
             source_changes = new_source_changes;
