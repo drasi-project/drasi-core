8724f72899172bc7216399a3b39de8e8e208e9a9
StandingMan <jmtangcs@gmail.com>
2026-02-14 08:20:52 +0000

    chore: register collect_list aggregation function for GQL
    Signed-off-by: StandingMan <jmtangcs@gmail.com>

--- functions-gql/src/lib.rs
+++ functions-gql/src/lib.rs
@@ -119,6 +119,10 @@ fn register_aggregation_functions(registry: &FunctionRegistry) {
     registry.register_function("min", Function::Aggregating(Arc::new(Min {})));
     registry.register_function("max", Function::Aggregating(Arc::new(Max {})));
     registry.register_function(
+        "collect_list",
+        Function::Aggregating(Arc::new(CollectList {})),
+    );
+    registry.register_function(
         "drasi.linearGradient",
         Function::Aggregating(Arc::new(LinearGradient {})),
     );
