a39fbd2607e941f02d00c2ad9652ea1f291b6411
Daniel Gerlag <daniel@gerlag.ca>
2026-03-06 23:21:49 +0000

    agentic generated reaction
    Signed-off-by: Daniel Gerlag <daniel@gerlag.ca>

---
+++ components/reactions/file/src/lib.rs
@@ -0,0 +1,34 @@
+#![allow(unexpected_cfgs)]
+// Copyright 2025 The Drasi Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! File reaction plugin for Drasi.
+
+mod config;
+pub mod descriptor;
+mod file;
+
+pub use config::{FileReactionConfig, QueryConfig, TemplateSpec, WriteMode};
+pub use file::{FileReaction, FileReactionBuilder};
+
+#[cfg(feature = "dynamic-plugin")]
+drasi_plugin_sdk::export_plugin!(
+    plugin_id = "file-reaction",
+    core_version = env!("CARGO_PKG_VERSION"),
+    lib_version = env!("CARGO_PKG_VERSION"),
+    plugin_version = env!("CARGO_PKG_VERSION"),
+    source_descriptors = [],
+    reaction_descriptors = [descriptor::FileReactionDescriptor],
+    bootstrap_descriptors = [],
+);
