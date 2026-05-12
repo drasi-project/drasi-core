8100662e43de57578806be2f449f7c8c3c75b34e
ahmed-kamal2004 <ahmedkamal200427@gmil.com>
2026-05-12 09:01:36 +0300

    shell-reaction: add testing suite
    Signed-off-by: ahmed-kamal2004 <ahmedkamal200427@gmil.com>
    Signed-off-by: ahmed-kamal2004 <ahmedkamal200427@gmail.com>

--- components/reactions/shell/tests/scripts/test1.sh
+++ components/reactions/shell/tests/scripts/test1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## FOR TESTING PURPOSES ONLY. DO NOT USE THIS FILE TO TEST THE COMPONENT. INSTEAD, USE THE INTEGRATION TESTS IN `integration_tests.rs`
+## FOR TESTING PURPOSES ONLY. USED WITHIN THE INTEGRATION TESTS IN `integration_tests.rs`
 
 # if STDIN_ENV_VAR is enabled, read the input
 if [ "$STDIN_ENV_VAR" = "true" ]; then
